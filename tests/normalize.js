19cc041c2d5c354c8ff1ad1ae5216874bca1b873
David Brown <dave@scri.pt>
2014-01-06 18:03:08 -0800

    Add field name/description normalization code, plus test cases. Add test cases for recursive normalization of field options.

--- tests/normalize.js
+++ tests/normalize.js
@@ -2,6 +2,7 @@
 var fs = require('fs'),
     wru = require('wru'),
     _ = require('underscore'),
+    jsdump = require('jsDump'),
     deepEqual = require('deep-equal'),
     n = require('../lib/normalize.js');
 
@@ -36,6 +37,44 @@ var compare_partial = function (_lhs, _rhs, _properties) {
 };
 
 /**
+ * @name check_fields:
+ */
+var check_fields = function (_name, _fields,
+                             _expected, _properties, _context) {
+
+  var context = (_context || []);
+
+  if (!_.isArray(_expected)) {
+    _expected = [ _expected ];
+  }
+
+  /* For each checkable object in scope */
+  for (var i = 0, len = _expected.length; i < len; ++i) {
+
+    if (_expected[i].type == 'fields') {
+
+      check_fields(
+        _name, (_fields[i].fields || []), (_expected[i].fields || []),
+          _properties, context.concat([ i ])
+      );
+
+    } else {
+
+      var path_text = (
+        context.length > 0 ?
+          ', along subfield path ' + JSON.stringify(context) : ''
+      );
+
+      wru.assert(
+        _name + path_text + ': properties ' +
+          JSON.stringify(_properties) + ' must match',
+        compare_partial(_expected[i], _fields[i], _properties)
+      );
+    }
+  }
+};
+
+/**
  * @name make_test:
  */
 var make_test = function (_name, _scope, _file) {
@@ -51,33 +90,20 @@ var make_test = function (_name, _scope, _file) {
     test: function () {
       _.each(tests, function (_test, _i) {
 
-        var h = 'Test #' + (_i + 1) + ' ';
+        var name = 'Test #' + (_i + 1);
         var from = _test.from, to = _test.to;
 
-        wru.assert(h + 'must have `to` property', _.isObject(to));
-        wru.assert(h + 'must have `from` property', _.isObject(from));
+        wru.assert(name + ' must have `to` property', _.isObject(to));
+        wru.assert(name + ' must have `from` property', _.isObject(from));
 
         n.normalize_forms(from);
 
         /* For each form */
         for (var i = 0, len = from.length; i < len; ++i) {
-
-          var check = _test.check;
-          var lhs = (to[i][_scope] || []);
-          var rhs = (from[i][_scope] || []);
-
-          if (!_.isArray(rhs)) {
-            rhs = [ rhs ];
-          }
-
-          /* For each checkable object in scope */
-          for (var j = 0, ln = lhs.length; j < ln; ++j) {
-
-            wru.assert(
-              h + 'properties ' + JSON.stringify(check) + ' must match',
-                compare_partial(lhs[j], rhs[j], _test.check)
-            );
-          }
+          check_fields(
+            name, (from[i][_scope] || []),
+              (to[i][_scope] || []), _test.check
+          );
         }
       });
     }
