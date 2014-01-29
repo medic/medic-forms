e81c82d3466b98a51cf2bb375f7dce07e809e019
David Brown <dave@scri.pt>
2014-01-29 02:03:37 -0800

    Form validation: validate all forms supplied.
    This modifies the behavior of the form validation code, so that it validates all
    forms provided to it, regardless of whether or not an earlier form saw an error.
    Like the input validation module, the form validator's return value now has a
    top-level `valid` property, plus a `detail` array with detailed information for
    each specific form that was validated.
    
    Note: Work in progress. For some reason, `wru` is silently failing to run some
    of the unit tests. Looking in to this now.

--- tests/validate.js
+++ tests/validate.js
@@ -3,28 +3,63 @@ var fs = require('fs'),
     wru = require('wru'),
     _ = require('underscore'),
     util = require('./util/util.js'),
-    v = require('../lib/validate.js'),
-    tests = require('./fixtures/compiled.js');
+    tests = require('./fixtures/compiled.js'),
+    form_validator = require('../lib/validate.js');
 
 
 /**
  * @name _assert
  */
-var _assert = function(_test, _i) {
+var _assert = function (_test, _i) {
+
+  var forms = _test.forms;
+  var test = 'Test #' + (_i + 1);
 
   var label = (
-    'Test #' + (_i + 1) +
-      ' must ' + (_test.valid ? '' : 'not ') + 'validate'
+    test + ' must ' +
+      (_test.valid ? '' : 'not ') + 'validate'
   );
 
-  v.validate_forms.call(
-    v, _test.forms,
-    wru.async(
-      label, function (_rv) {
-        wru.assert(label, (_rv.valid === _test.valid))
+  form_validator.validate_forms(forms,
+    wru.async(label, function (_rv) {
+
+      /* Paranoia:
+       *   These assertions test the test fixtures. */
+
+      wru.assert(
+        test + ' must provide an array for the `forms` property',
+          _.isArray(forms)
+      );
+
+      /* Check top-level validity:
+       *   This is an all-or-nothing result for all form
+       *   definitions provided by the current test fixture. */
+
+      wru.assert(label, (_rv.valid === _test.valid));
+
+      /* Check individual form validity:
+       *   We only do this if the `all` parameter is present, indicating
+       *   that the `valid` property should match the validity of *all*
+       *   forms in the test fixture, not just the top-level result. */
+
+      if (_test.all) {
+
+        var detail = _rv.detail;
+
+        wru.assert(
+          test + ' must have one detailed result for every form',
+            (forms.length == detail.length)
+        );
+
+        for (var i = 0, len = detail.length; i < len; ++i) {
+          wru.assert(
+            label + ' at offset ' + i,
+              (detail[i].valid === _test.valid)
+          );
+        }
       }
-    )
-  );
+    }
+  ));
 }
 
 
