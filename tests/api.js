3185babea9d59a9e49169e6ce1f1f4ab90a5eff4
David Brown <dave@scri.pt>
2014-02-17 18:55:51 -0800

    Reorganize API test harness and fixtures in preparation for testing input validation.

--- tests/api.js
+++ tests/api.js
@@ -1,9 +1,10 @@
 
 var fs = require('fs'),
+    async = require('async'),
     _ = require('underscore'),
+    jsdump = require('jsDump'),
     api = require('../lib/api'),
     util = require('./include/util'),
-    jsdump = require('jsDump'),
     fixtures = require('./fixtures/compiled');
 
 
@@ -12,28 +13,90 @@ var fs = require('fs'),
  */
 var _assert = function (_test, _fixture) {
 
-  var forms = _fixture.forms;
+  /* Top-level fixtures */
+  var fixtures = _fixture.fixtures;
   var expect = _fixture.expect;
 
-  /* Sanity check for fixture */
-  _test.ok(_.isObject(forms), 'must have valid `forms` property');
-  _test.ok(_.isObject(expect), 'must have valid `expect` property');
+  _test.ok(
+    _.isObject(fixtures),
+      'must have valid `fixtures` property'
+  );
 
-  api.create({}, function (_result) {
+  _test.ok(
+    _.isObject(expect),
+      'must have valid `expect` property'
+  );
 
-    /* Sanity checks for `create` API */
-    _test.ok(_.isObject(this), 'must initialize properly');
-    _test.ok(_.isObject(_result), 'must yield a result object');
+  /* Form-specific fixtures */
+  var forms = fixtures.forms;
+  var expect_forms = expect.forms;
+
+  _test.ok(
+    _.isObject(forms),
+      'must have valid `fixtures.forms` property'
+  );
+
+  _test.ok(
+    _.isObject(expect_forms),
+      'must have valid `expect.forms` property'
+  );
+
+  /* Input-specific fixtures */
+  var input = fixtures.input;
+  var expect_input = expect.input;
+
+  _test.ok(
+    _.isObject(input),
+      'must have valid `fixtures.input` property'
+  );
+
+  _test.ok(
+    _.isObject(expect_input),
+      'must have valid `expect.input` property'
+  );
+
+  /* Test API */
+  return async.waterfall([
+
+    /* Test `create` API */
+    function (_next_fn) {
+
+      api.create({}, function (_rv) {
+
+        /* Sanity checks */
+        _test.ok(_.isObject(this), 'must initialize properly');
+        _test.ok(_.isObject(_rv), 'must yield a result object');
+
+        return _next_fn(null, this);
+      });
+    },
 
     /* Test `load` API */
-    this.load(forms, function (_rv) {
-      _test.ok(
-        util.is_recursive_subset(expect, _rv),
-          'should produce expected result object'
-      );
-      _test.done();
-    });
+    function (_api, _next_fn) {
+
+      _api.load(forms, function (_rv) {
+
+        _test.ok(
+          util.is_recursive_subset(expect.forms, _rv),
+            'should produce expected result object'
+        );
+
+        return _next_fn(null, _api);
+      });
+    },
+
+    /* Test `fill` API */
+    function (_api, _next_fn) {
+
+      return _next_fn();
+    }
+
+  ], function (_err) {
+
+    /* Finished */
+    _test.done();
   });
+  
 }
 
 /* Tests */
