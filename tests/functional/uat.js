dbf2f73c616cd3909ae2a04e63a00e5e3b70d424
Gareth Bowen <gareth@bowenwebdesign.co.nz>
2014-04-23 14:44:31 +1200

    Form demo links are now generated from the example definitions to make it easy to add examples

--- tests/functional/uat.js
+++ tests/functional/uat.js
@@ -1,103 +1,102 @@
 $(function() {
 
-  var definitions = {
-    simple: {
-      "meta": {
-        "id": "TEST"
-      },
-      "fields": [
-        {
-          "id": "name",
-          "name": "Patient Name",
-          "type": "string"
-        }
-      ]
+  var definitions = {};
+  definitions['Single string field'] = {
+    "meta": {
+      "id": "TEST"
     },
-    repeating: {
-      "meta": {
-        "id": "TEST"
-      },
-      "fields": [
-        {
-          "id": "name",
-          "name": "Patient Name",
-          "type": "string",
-          "repeat": true
-        }
-      ]
+    "fields": [
+      {
+        "id": "name",
+        "name": "Patient Name",
+        "type": "string"
+      }
+    ]
+  };
+  definitions['Repeating string field'] = {
+    "meta": {
+      "id": "TEST"
     },
-    required: {
-      "meta": {
-        "id": "TEST"
-      },
-      "fields": [
-        {
-          "id": "name",
-          "name": "Patient Name",
-          "type": "string",
-          "required": true
-        }
-      ]
+    "fields": [
+      {
+        "id": "name",
+        "name": "Patient Name",
+        "type": "string",
+        "repeat": true
+      }
+    ]
+  };
+  definitions['Required string field'] = {
+    "meta": {
+      "id": "TEST"
     },
-    nested: {
-      "meta": {
-        "id": "TEST"
-      },
-      "fields": [
-        {
-          "id": "name",
-          "name": "Name",
-          "type": "string"
-        },
-        {
-          "id": "address",
-          "name": "Address",
-          "type": "fields",
-          "fields": [
-            {
-              "id": "street",
-              "name": "Street",
-              "type": "string"
-            },
-            {
-              "id": "city",
-              "name": "City",
-              "type": "string"
-            }
-          ]
-        }
-      ]
+    "fields": [
+      {
+        "id": "name",
+        "name": "Patient Name",
+        "type": "string",
+        "required": true
+      }
+    ]
+  };
+  definitions['Nested string fields'] = {
+    "meta": {
+      "id": "TEST"
     },
-    repeatingNested: {
-      "meta": {
-        "id": "TEST"
+    "fields": [
+      {
+        "id": "name",
+        "name": "Name",
+        "type": "string"
       },
-      "fields": [
-        {
-          "id": "name",
-          "name": "Name",
-          "type": "string"
-        },
-        {
-          "id": "address",
-          "name": "Address",
-          "type": "fields",
-          "repeat": true,
-          "fields": [
-            {
-              "id": "street",
-              "name": "Street",
-              "type": "string"
-            },
-            {
-              "id": "city",
-              "name": "City",
-              "type": "string"
-            }
-          ]
-        }
-      ]
-    }
+      {
+        "id": "address",
+        "name": "Address",
+        "type": "fields",
+        "fields": [
+          {
+            "id": "street",
+            "name": "Street",
+            "type": "string"
+          },
+          {
+            "id": "city",
+            "name": "City",
+            "type": "string"
+          }
+        ]
+      }
+    ]
+  };
+  definitions['Repeating nested string fields'] = {
+    "meta": {
+      "id": "TEST"
+    },
+    "fields": [
+      {
+        "id": "name",
+        "name": "Name",
+        "type": "string"
+      },
+      {
+        "id": "address",
+        "name": "Address",
+        "type": "fields",
+        "repeat": true,
+        "fields": [
+          {
+            "id": "street",
+            "name": "Street",
+            "type": "string"
+          },
+          {
+            "id": "city",
+            "name": "City",
+            "type": "string"
+          }
+        ]
+      }
+    ]
   };
 
   $('body').on('click', '#shortcuts a', function (e) {
@@ -105,8 +104,12 @@ $(function() {
     e.preventDefault();
     e.stopPropagation();
 
-    var form = definitions[$(e.target).data('definition')];
+    var form = definitions[$(e.target).text()];
     $('[name=formDefinition]').val(JSON.stringify(form, null, '  '));
   });
+
+  for (definition in definitions) {
+    $('#shortcuts').append('<li><a href="#">' + definition + '</a></li>');
+  }
 
 });
