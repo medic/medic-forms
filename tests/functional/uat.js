e4e24f3d012f6216f6c7056dd289a1a57e886472
Gareth Bowen <gareth@bowenwebdesign.co.nz>
2014-06-03 15:32:03 +1200

    Pulled range and length out into validations section

--- tests/functional/uat.js
+++ tests/functional/uat.js
@@ -249,116 +249,6 @@ $(function() {
             "type": "string"
           },
           {
-            "id": "length",
-            "name": "Length",
-            "type": "fields",
-            "fields": [
-              {
-                "id": "length-type",
-                "name": "Length Validation",
-                "type": "select",
-                "default": "none",
-                "items": [
-                  ["none","None"],
-                  ["minimum","Minimum"],
-                  ["maximum","Maximum"],
-                  ["between","Between"],
-                  ["script","Scripted"]
-                ]
-              },
-              {
-                "id": "length-minimum",
-                "name": "Minimum",
-                "type": "number",
-                "conditions": {
-                  "structured": {
-                    "fields.length.length-type": ["minimum","between"]
-                  }
-                }
-              },
-              {
-                "id": "length-maximum",
-                "name": "Maximum",
-                "type": "number",
-                "conditions": {
-                  "structured": {
-                    "fields.length.length-type": ["maximum","between"]
-                  }
-                }
-              },
-              {
-                "id": "length-script",
-                "name": "Script",
-                "type": "string",
-                "conditions": {
-                  "structured": {
-                    "fields.length.length-type": ["script"]
-                  }
-                }
-              }
-            ],
-            "conditions": {
-              "structured": {
-                "fields.type": ["string","email","json"]
-              }
-            }
-          },
-          {
-            "id": "range",
-            "name": "Range Validation",
-            "type": "fields",
-            "fields": [
-              {
-                "id": "range-type",
-                "name": "Valid Range",
-                "type": "select",
-                "default": "none",
-                "items": [
-                  ["none","None"],
-                  ["minimum","Minimum"],
-                  ["maximum","Maximum"],
-                  ["between","Between"],
-                  ["script","Scripted"]
-                ]
-              },
-              {
-                "id": "range-minimum",
-                "name": "Minimum",
-                "type": "number",
-                "conditions": {
-                  "structured": {
-                    "fields.range.range-type": ["minimum","between"]
-                  }
-                }
-              },
-              {
-                "id": "range-maximum",
-                "name": "Maximum",
-                "type": "number",
-                "conditions": {
-                  "structured": {
-                    "fields.range.range-type": ["maximum","between"]
-                  }
-                }
-              },
-              {
-                "id": "range-script",
-                "name": "Script",
-                "type": "string",
-                "conditions": {
-                  "structured": {
-                    "fields.range.range-type": ["script"]
-                  }
-                }
-              }
-            ],
-            "conditions": {
-              "structured": {
-                "fields.type": ["integer","number"]
-              }
-            }
-          },
-          {
             "id": "required",
             "name": "Required",
             "type": "fields",
@@ -497,31 +387,148 @@ $(function() {
             ]
           },
           {
-            "id": "validations",
+            "id": "validation-section",
             "name": "Validations",
             "type": "fields",
             "fields": [
               {
-                "id": "validations-operator",
-                "name": "Operator",
-                "type": "select",
-                "default": "&&",
-                "items": [
-                  ["&&","And"],
-                  ["||","Or"]
+                "id": "length",
+                "name": "Length",
+                "type": "fields",
+                "fields": [
+                  {
+                    "id": "length-type",
+                    "name": "Length Validation",
+                    "type": "select",
+                    "default": "none",
+                    "items": [
+                      ["none","None"],
+                      ["minimum","Minimum"],
+                      ["maximum","Maximum"],
+                      ["between","Between"],
+                      ["script","Scripted"]
+                    ]
+                  },
+                  {
+                    "id": "length-minimum",
+                    "name": "Minimum",
+                    "type": "number",
+                    "conditions": {
+                      "structured": {
+                        "fields.validation-section.length.length-type": ["minimum","between"]
+                      }
+                    }
+                  },
+                  {
+                    "id": "length-maximum",
+                    "name": "Maximum",
+                    "type": "number",
+                    "conditions": {
+                      "structured": {
+                        "fields.validation-section.length.length-type": ["maximum","between"]
+                      }
+                    }
+                  },
+                  {
+                    "id": "length-script",
+                    "name": "Script",
+                    "type": "string",
+                    "conditions": {
+                      "structured": {
+                        "fields.validation-section.length.length-type": ["script"]
+                      }
+                    }
+                  }
+                ],
+                "conditions": {
+                  "structured": {
+                    "fields.type": ["string","email","json"]
+                  }
+                }
+              },
+              {
+                "id": "range",
+                "name": "Range Validation",
+                "type": "fields",
+                "fields": [
+                  {
+                    "id": "range-type",
+                    "name": "Valid Range",
+                    "type": "select",
+                    "default": "none",
+                    "items": [
+                      ["none","None"],
+                      ["minimum","Minimum"],
+                      ["maximum","Maximum"],
+                      ["between","Between"],
+                      ["script","Scripted"]
+                    ]
+                  },
+                  {
+                    "id": "range-minimum",
+                    "name": "Minimum",
+                    "type": "number",
+                    "conditions": {
+                      "structured": {
+                        "fields.validation-section.range.range-type": ["minimum","between"]
+                      }
+                    }
+                  },
+                  {
+                    "id": "range-maximum",
+                    "name": "Maximum",
+                    "type": "number",
+                    "conditions": {
+                      "structured": {
+                        "fields.validation-section.range.range-type": ["maximum","between"]
+                      }
+                    }
+                  },
+                  {
+                    "id": "range-script",
+                    "name": "Script",
+                    "type": "string",
+                    "conditions": {
+                      "structured": {
+                        "fields.validation-section.range.range-type": ["script"]
+                      }
+                    }
+                  }
+                ],
+                "conditions": {
+                  "structured": {
+                    "fields.type": ["integer","number"]
+                  }
+                }
+              },
+              {
+                "id": "validations",
+                "name": "Custom Validations",
+                "type": "fields",
+                "fields": [
+                  {
+                    "id": "validations-operator",
+                    "name": "Operator",
+                    "type": "select",
+                    "default": "&&",
+                    "items": [
+                      ["&&","And"],
+                      ["||","Or"]
+                    ]
+                  },
+                  {
+                    "id": "validations-javascript",
+                    "name": "Scripted",
+                    "type": "string",
+                    "repeat": true
+                  },
+                  {
+                    "id": "validations-registered",
+                    "name": "Registered",
+                    "type": "string",
+                    "repeat": true
+                  }
                 ]
-              },
-              {
-                "id": "validations-javascript",
-                "name": "Scripted",
-                "type": "string",
-                "repeat": true
-              },
-              {
-                "id": "validations-registered",
-                "name": "Registered",
-                "type": "string",
-                "repeat": true
               }
             ]
           }
